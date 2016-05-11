f9359c1976d5a95b82957faeff9016fbbc17816f
jodusnodus <thomas842@hotmail.com>
2016-05-11 17:10:52 +0200

    Update

--- src/tray.js
+++ src/tray.js
@@ -2,7 +2,8 @@ import { app, Tray, Menu, shell } from "electron"
 import path from "path"
 import Syncthing from "node-syncthing"
 import { notify, formatBytes } from "./misc"
-import { myID, config, connections, folderStatus } from "./actions"
+import { myID } from "./actions"
+import { stateHandler, events } from "./events"
 
 const stConfig = {
   hostname: "localhost",
@@ -28,45 +29,50 @@ const actions = {
   }
 }
 
+const folder = ({ id, status, path }) => ({
+  label: `${id} ${status ? "(" + formatBytes(status.inSyncBytes) + "/" + formatBytes(status.globalBytes) + ") ("+ status.state +")": ""}`,
+  id,
+  path, 
+  click: ({path}) => shell.showItemInFolder(path)
+})
+
+const device = ({ deviceID, name, online, address }) => ({
+  label: `${name} ${online ? "("+address+")" : "(disconnected)"}`,
+  id: deviceID,
+  type: "checkbox",
+  enabled: online ? true : false
+})
+
+const folderItems = folders => folders.length > 0 ? [
+  { label: "Folders", enabled: false  },
+  ...folders.map(folder)
+] : [
+  { label: "No folders found", enabled: false }
+]
+
+const devicesItems = devices => devices.length > 0 ? [
+  { label: "Devices", enabled: false },
+  ...devices.map(device)
+] : [{ label: "No devices found", enabled: false }]
+
 function buildTray({devices, folders, connected}){
-  const folderItems = folders.length > 0 ? [
-    { label: "Folders", enabled: false  },
-    ...folders.map(({ id, status, path }) => {
-      return {
-        label: `${id} ${status ? "(" + formatBytes(status.inSyncBytes) + "/" + formatBytes(status.globalBytes) + ")" : ""}`,
-        path, 
-        click: ({path}) => shell.showItemInFolder(path)
-      }
-    })
-  ] : [
-    { label: "No folders found", enabled: false }
-  ]
-
-  const devicesItems = devices.length > 0 ? [
-    { label: "Devices", enabled: false },
-    ...devices.map(({ name, online, address }) => ({
-      label: `${name} ${online ? "("+address+")" : "(offline)"}`,
-      type: "checkbox",
-      enabled: online
-    }))
-  ] : [{ label: "No devices found", enabled: false }]
-
   let menu = null;
+
   if(connected){
     menu = Menu.buildFromTemplate([
-      ...folderItems,
+      ...folderItems(folders),
       { type: "separator" },
-      ...devicesItems,
+      ...devicesItems(devices),
       { type: "separator" },
-      { label: "Dashboard", click: actions.dashboard },
+      { label: "Dashboard...", click: actions.dashboard },
       { type: "separator" },
-      { label: 'Restart', click: actions.restart },
-      { label: 'Quit', click: actions.quit }
+      { label: 'Restart Syncthing', click: actions.restart },
+      { label: 'Quit Syncthing', click: actions.quit }
     ])
   }else{
     menu = Menu.buildFromTemplate([
       { label: "Connection error", enabled: false },
-      { label: 'Quit', click: actions.quit }
+      { label: 'Quit Syncthing', click: actions.quit }
     ])
   }
   return menu
@@ -74,59 +80,17 @@ function buildTray({devices, folders, connected}){
 
 let tray = null;
 export default function TrayWrapper(store){
-  tray = new Tray(path.join(__dirname, "../icon@4x.png"))
-
-  let previousState = store.getState()
-
-  //Subscribe to state changes
-  store.subscribe(() => {
-    const newState = store.getState()
-    if(previousState.devices.length !== newState.devices.length)
-      store.dispatch(connections(st))
-
-    if(!previousState.myID && newState.myID)
-      store.dispatch(config(newState.myID, st))
-
-    if(previousState.folders.length !== newState.folders.length)
-      store.dispatch(folderStatus(newState.folders, st))
-
-    const menu = buildTray(newState)
-    tray.setContextMenu(menu)
-    previousState = newState
-  })
+  tray = new Tray(path.join(__dirname, "../tray_white@4x.png"))
 
   const menu = buildTray(store.getState())
+  //Subscribe to state changes
+  store.subscribe(stateHandler(menu, store, st, tray, buildTray))
+
+
   tray.setContextMenu(menu)
+
   store.dispatch(myID(st))
 
   //Listen
-  eventListener(st, store)
-}
-
-function eventListener(st, store){
-  st.on("deviceConnected", ({ id, addr }) => {
-    const { name } = store.getState().devices.filter(device => device.deviceID == id)[0]
-    notify(`Connected to ${name}`, `on ${addr}`)
-    store.dispatch(connections(st))
-  })
-  st.on("deviceDisconnected", ({data: {id}}) => {
-    const { name } = store.getState().devices.filter(device => device.deviceID == id)[0]
-    notify(`${name} disconnected`)
-    store.dispatch(connections(st))
-  })
-  st.on("stateChanged", ({ folder, from, to }) => {
-    const state = store.getState()
-    switch (to) {
-      case 'syncing':
-        notify(`${folder} is Syncing`)
-        store.dispatch(folderStatus(state.folders, st))
-        break;
-      case 'error':
-        notify(`${folder} has an Error`, 'click to see the error in the dashboard.')
-        break;
-      case 'idle':
-        store.dispatch(folderStatus(state.folders, st))
-        break;
-    }
-  })
+  events(st, store)
 }
