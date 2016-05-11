6e157cde3b29d332642b4f760614ddd95a1a0935
jodusnodus <thomas842@hotmail.com>
2016-05-11 11:50:31 +0200

    Add stateChange event listener

--- src/tray.js
+++ src/tray.js
@@ -8,7 +8,7 @@ const stConfig = {
   hostname: "localhost",
   port: 8384,
   apiKey: "19A5pMYGNr0SOqzL9xoyJp7JuRbwzZq2",
-  eventListener: false
+  eventListener: true 
 }
 
 const st = new Syncthing(stConfig)
@@ -22,6 +22,9 @@ const actions = {
   quit(){
     st.system.shutdown().then(() => {})
     app.quit()
+  },
+  dashboard(){
+    shell.openExternal(`${stConfig.https ? "https" : "http"}://${stConfig.hostname}:${stConfig.port}`) 
   }
 }
 
@@ -55,7 +58,8 @@ function buildTray({devices, folders, connected}){
       { type: "separator" },
       ...devicesItems,
       { type: "separator" },
-      { label: "Dashboard", click: () => shell.openExternal(`${stConfig.https ? "https" : "http"}://${stConfig.hostname}:${stConfig.port}`) },
+      { label: "Dashboard", click: actions.dashboard },
+      { type: "separator" },
       { label: 'Restart', click: actions.restart },
       { label: 'Quit', click: actions.quit }
     ])
@@ -73,6 +77,8 @@ export default function TrayWrapper(store){
   tray = new Tray(path.join(__dirname, "../icon@4x.png"))
 
   let previousState = store.getState()
+
+  //Subscribe to state changes
   store.subscribe(() => {
     const newState = store.getState()
     if(previousState.devices.length !== newState.devices.length)
@@ -92,4 +98,35 @@ export default function TrayWrapper(store){
   const menu = buildTray(store.getState())
   tray.setContextMenu(menu)
   store.dispatch(myID(st))
+
+  //Listen
+  eventListener(st, store)
+}
+
+function eventListener(st, store){
+  st.on("deviceConnected", ({ id, addr }) => {
+    const { name } = store.getState().devices.filter(device => device.deviceID == id)[0]
+    notify(`Connected to ${name}`, `on ${addr}`)
+    store.dispatch(connections(st))
+  })
+  st.on("deviceDisconnected", ({data: {id}}) => {
+    const { name } = store.getState().devices.filter(device => device.deviceID == id)[0]
+    notify(`${name} disconnected`)
+    store.dispatch(connections(st))
+  })
+  st.on("stateChanged", ({ folder, from, to }) => {
+    const state = store.getState()
+    switch (to) {
+      case 'syncing':
+        notify(`${folder} is Syncing`)
+        store.dispatch(folderStatus(state.folders, st))
+        break;
+      case 'error':
+        notify(`${folder} has an Error`, 'click to see the error in the dashboard.')
+        break;
+      case 'idle':
+        store.dispatch(folderStatus(state.folders, st))
+        break;
+    }
+  })
 }
