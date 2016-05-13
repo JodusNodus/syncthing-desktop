a20e253a37b44a631cc38a9b38be1a22bfa05086
jodusnodus <thomas842@hotmail.com>
2016-05-13 22:48:37 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import electronDebug from "electron-debug"
 
 import Tray from "./tray"
 import configureStore from "./store"
-import { notify } from "./misc"
 
 electronDebug()
 
 app.on("ready", () => {
   const store = configureStore()
-  const tray = new Tray(store)
+  new Tray(store)
 })
