ed291c19445bb73ef11570341bfc758fa5a02000
jodusnodus <thomas842@hotmail.com>
2016-07-05 00:23:28 +0200

    Device & Folder page state

--- app/client/containers/Device/index.js
+++ app/client/containers/Device/index.js
@@ -1,8 +1,27 @@
 import React, { PropTypes, Component } from 'react'
 import h from 'react-hyperscript'
+import { connect } from 'react-redux'
 
-export default class Device extends Component {
+class Device extends Component {
   render(){
-    return h('h1', 'Device Page')
+    const { devices, params } = this.props
+    const device = devices.filter(x => x.deviceID == params.id)[0]
+
+    return device ? h('div', [ 
+      h('h1', device.name),
+    ]) : h('div', [
+      h('h1', 'Device not available'),
+    ])
   }
 }
+
+Device.propTypes = {
+  params: PropTypes.object.isRequired,
+  devices: PropTypes.array.isRequired,
+}
+
+export default connect(
+  state => ({
+    devices: state.devices,
+  })
+)(Device)
--- app/client/containers/Folder/index.js
+++ app/client/containers/Folder/index.js
@@ -1,8 +1,27 @@
 import React, { PropTypes, Component } from 'react'
 import h from 'react-hyperscript'
+import { connect } from 'react-redux'
 
-export default class Folder extends Component {
+class Folder extends Component {
   render(){
-    return h('h1', 'Folder Page')
+    const { folders, params } = this.props
+    const folder = folders.filter(x => x.id == params.id)[0]
+
+    return folder ? h('div', [ 
+      h('h1', folder.id),
+    ]) : h('div', [
+      h('h1', 'Folder not available'),
+    ])
   }
 }
+
+Folder.propTypes = {
+  params: PropTypes.object.isRequired,
+  folders: PropTypes.array.isRequired,
+}
+
+export default connect(
+  state => ({
+    folders: state.folders,
+  })
+)(Folder)
