8762a913c56b9ce561af2bb7f0e3f7972f1a4542
jodusnodus <thomas842@hotmail.com>
2016-07-07 19:38:07 +0200

    Use binary unit

--- app/main/misc.js
+++ app/main/misc.js
@@ -25,7 +25,7 @@ export function notify(...data){
 
 export function formatBytes(bytes, decimals=0) {
   if(bytes == 0) return '0 Byte'
-  var k = 1000 // or 1024 for binary
+  var k = 1024
   var dm = decimals + 1 || 3
   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
   var i = Math.floor(Math.log(bytes) / Math.log(k))
