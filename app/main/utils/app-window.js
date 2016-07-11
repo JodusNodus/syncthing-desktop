646362216bf9a3f269eb4e7223c288561bf98f2a
jodusnodus <thomas842@hotmail.com>
2016-07-11 20:30:10 +0200

    Don't create new windows if already open

--- app/main/utils/app-window.js
+++ app/main/utils/app-window.js
@@ -1,13 +1,28 @@
-import { BrowserWindow } from 'electron'
+import { app, BrowserWindow } from 'electron'
 
+let win
 export default function appWindow(){
-  const win = new BrowserWindow({
-    show: false,
-    titleBarStyle: 'hidden-inset',
-    minHeight: 400,
-    minWidth: 600,
-    height: 550,
-    width: 800,
-  })
-  win.loadURL(`file://${__dirname}/../../../app/client/app.html`)
+  if(win){
+    if(win.isMinimized())
+      win.restore()
+    if(win.isFullScreen())
+      win.setFullscreen(false)
+    win.focus()
+  }else{
+    win = new BrowserWindow({
+      show: false,
+      titleBarStyle: 'hidden-inset',
+      minHeight: 400,
+      minWidth: 600,
+      height: 550,
+      width: 800,
+    })
+    win.loadURL(`file://${__dirname}/../../../app/client/app.html`)
+
+    win.on('close', () => {
+      app.dock.hide()
+      win = null
+    })
+
+  }
 }
