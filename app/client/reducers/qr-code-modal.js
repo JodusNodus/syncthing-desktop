3c0e200651f6c45b7c955484346203d809a3df61
jodusnodus <thomas842@hotmail.com>
2016-07-11 21:42:18 +0200

    Add comments

--- app/client/reducers/qr-code-modal.js
+++ app/client/reducers/qr-code-modal.js
@@ -11,6 +11,12 @@ export default function qrCodeModal(state=initialState, {type, payload}) {
       qrCode: payload,
     }
     case 'QR_CODE_MODAL_HIDE':
+      /*
+       * notice that qrCode stays the same,
+       * this is beacuse the hiding is animated and removing the qr code
+       * would result in a sudden change or disappearance of the qr code
+       * while the animation is still going.
+       */
       return {
       ...state,
       show: false,
