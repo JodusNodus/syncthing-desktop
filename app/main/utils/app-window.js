d1572b6fcc2fb6655550a72179efec0a72a39921
jodusnodus <thomas842@hotmail.com>
2016-07-11 20:16:08 +0200

    Better defaults

--- app/main/utils/app-window.js
+++ app/main/utils/app-window.js
@@ -4,6 +4,10 @@ export default function appWindow(){
   const win = new BrowserWindow({
     show: false,
     titleBarStyle: 'hidden-inset',
+    minHeight: 400,
+    minWidth: 600,
+    height: 550,
+    width: 800,
   })
   win.loadURL(`file://${__dirname}/../../../app/client/app.html`)
 }
