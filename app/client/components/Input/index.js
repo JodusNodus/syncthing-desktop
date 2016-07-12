e764e89944a316b7cf87e8ffeb0eab446823d23b
jodusnodus <thomas842@hotmail.com>
2016-07-12 23:33:27 +0200

    Array input value wil return return array

--- app/client/components/Input/index.js
+++ app/client/components/Input/index.js
@@ -1,12 +1,27 @@
 import { PropTypes } from 'react'
 import h from 'react-hyperscript'
+import _ from 'lodash'
 
 import { styles } from './styles.scss'
 
-const Input = ({ value, label, className='', touched, error, ...args}) =>
+const handleChange = (initialValue, onChange) => e => {
+  const value = e.target.value
+
+  if(_.isArray(initialValue)){
+    const valueArray = value
+    .split(',')
+    .map(str => str.trim())
+
+    onChange(valueArray)
+  }else{
+    onChange(value)
+  }
+}
+
+const Input = ({ value, onChange, label, className='', touched, error, initialValue, ...args}) =>
 h('div.form-group', {className: `${styles} ${touched && error && 'error'} ${className}`}, [
   label && h('label', label),
-  h('input.form-control', {value, ...args}),
+  h('input.form-control', {value, onChange: handleChange(initialValue, onChange), ...args}),
 ])
 
 Input.propTypes = {
@@ -14,7 +29,8 @@ Input.propTypes = {
   className: PropTypes.string,
   touched: PropTypes.bool,
   error: PropTypes.string,
-  value: PropTypes.string.isRequired,
+  value: PropTypes.any.isRequired,
+  initialValue: PropTypes.any.isRequired,
 }
 
 export default Input
