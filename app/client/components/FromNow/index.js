da8bb06bbc1eecf668bf5b7134583cd77647ad8f
jodusnodus <thomas842@hotmail.com>
2016-07-22 17:32:48 +0200

    Fix FromNow

--- app/client/components/FromNow/index.js
+++ app/client/components/FromNow/index.js
@@ -2,12 +2,14 @@ import { Component, PropTypes } from 'react'
 import h from 'react-hyperscript'
 import moment from 'moment'
 
-import { styles } from './styles.scss'
+const capitalizeFirstLetter = str => [
+  str.charAt(0).toUpperCase(),
+  str.slice(1),
+].join('')
 
 export default class FromNow extends Component {
   static propTypes = {
     value: PropTypes.string.isRequired,
-    className: PropTypes.string,
   }
   componentDidMount(){
     //Force redraw every 5 seconds
@@ -19,7 +21,10 @@ export default class FromNow extends Component {
     clearInterval(this.interval)
   }
   render(){
-    const { value, className='', ...args } = this.props
-    return h('span', {className: `${styles} ${className}`, ...args}, moment('0001').isSame(value) ? 'Never' : moment(value).fromNow())
+    const { value, ...args } = this.props
+    const text = moment(value).fromNow()
+    const isNotValid = moment('0001').isSame(value) || moment(0).isSame(value)
+
+    return h('span', args, isNotValid ? 'Never' : capitalizeFirstLetter(text))
   }
 }
